d4bd4d1fd25b36d263bc156698b6d7df90ba7d0b
Tristan Weech <twminebro@gmail.com>
2025-03-31 14:26:03 -0600

    BishopCalculator is all set up and passing now.

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -69,6 +69,9 @@ public class ChessPiece {
             case ROOK:
                 moveCal = new RookCalculator();
                 break;
+            case BISHOP:
+                moveCal = new BishopCalculator();
+                break;
             default:
                 return null;
         }
