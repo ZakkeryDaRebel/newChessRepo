477d9a17ba0de7f951f243ded6c8f158c26b3edb
Tristan Weech <twminebro@gmail.com>
2025-06-24 13:35:58 -0600

    After comparing my repository with GitHub's starter code, I changed a couple things and now I have the javalin server up and running.

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,44 +1,24 @@
 package server;
 
-import spark.*;
+import io.javalin.*;
 
 public class Server {
 
-    //RegisterHandler registerHandler;
-    //LoginHandler loginHandler;
-    //LogoutHandler logoutHandler;
-    //CreateGameHandler createGameHandler;
-    //ListGamesHandler listGamesHandler;
-    //JoinGameHandler joinGameHandler;
-    //clearHandler clearHandler;
+    private final Javalin javalin;
 
     public Server() {
+        javalin = Javalin.create(config -> config.staticFiles.add("web"));
 
-        //registerHandler = new RegisterHandler();
-        //loginHandler = new LoginHandler();
-        //logoutHandler = new LogoutHandler();
-        //createGameHandler = new CreateGameHandler();
-        //listGamesHandler = new ListGamesHandler();
-        //joinGameHandler = new JoinGameHandler();
-        //clearHandler = new clearHandler();
+        // Register your endpoints and exception handlers here.
+
     }
 
     public int run(int desiredPort) {
-        Spark.port(desiredPort);
-
-        Spark.staticFiles.location("web");
-
-        // Register your endpoints and handle exceptions here.
-
-        //This line initializes the server and can be removed once you have a functioning endpoint 
-        Spark.init();
-
-        Spark.awaitInitialization();
-        return Spark.port();
+        javalin.start(desiredPort);
+        return javalin.port();
     }
 
     public void stop() {
-        Spark.stop();
-        Spark.awaitStop();
+        javalin.stop();
     }
 }
