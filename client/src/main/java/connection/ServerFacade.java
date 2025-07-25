a00a545c4e11973a18d379ab69eac421cb9ab9dc
Tristan Weech <twminebro@gmail.com>
2025-07-25 13:33:52 -0600

    Started to have ServerFacade's functionality work with Javalin

---
+++ client/src/main/java/connection/ServerFacade.java
@@ -0,0 +1,96 @@
+package connection;
+
+import com.google.gson.Gson;
+import exception.ResponseException;
+import requests.*;
+import results.*;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ServerFacade {
+
+    private String serverURL;
+    private HttpClient client = HttpClient.newHttpClient();
+
+    public ServerFacade(String serverURL) {
+        this.serverURL = serverURL;
+    }
+
+    public RegisterResult register(RegisterRequest request) throws ResponseException {
+        HttpRequest httpRequest = buildRequest("POST", "/user", "", request);
+        HttpResponse<String> httpResponse = sendRequest(httpRequest);
+        return handleResponse(httpResponse, RegisterResult.class);
+    }
+
+    public LoginResult login(LoginRequest request) throws ResponseException {
+        HttpRequest httpRequest = buildRequest("POST", "/session", "", request);
+        HttpResponse<String> httpResponse = sendRequest(httpRequest);
+        return handleResponse(httpResponse, LoginResult.class);
+    }
+
+    public void logout(LogoutRequest request) throws ResponseException {
+        HttpRequest httpRequest = buildRequest("DELETE", "/session", request.authToken(), null);
+        HttpResponse<String> httpResponse = sendRequest(httpRequest);
+        handleResponse(httpResponse, null);
+    }
+
+    public CreateGameResult createGame(CreateGameRequest request) throws ResponseException {
+        HttpRequest httpRequest = buildRequest("POST", "/game", request.authToken(), request);
+        HttpResponse<String> httpResponse = sendRequest(httpRequest);
+        return handleResponse(httpResponse, CreateGameResult.class);
+    }
+
+    public ListGamesResult listGames(ListGamesRequest request) throws ResponseException {
+        HttpRequest httpRequest = buildRequest("GET", "/game", request.authToken(), null);
+        HttpResponse<String> httpResponse = sendRequest(httpRequest);
+        return handleResponse(httpResponse, ListGamesResult.class);
+    }
+
+    public void joinGame(JoinGameRequest request) throws ResponseException {
+        HttpRequest httpRequest = buildRequest("PUT", "/game", request.authToken(), request);
+        HttpResponse<String> httpResponse = sendRequest(httpRequest);
+        handleResponse(httpResponse, null);
+    }
+
+    public void clear() throws ResponseException {
+        HttpRequest httpRequest = buildRequest("DELETE", "/db", "", null);
+        HttpResponse<String> httpResponse = sendRequest(httpRequest);
+        handleResponse(httpResponse, null);
+    }
+
+
+    public HttpRequest buildRequest(String method, String path, String authToken, Object body) {
+        HttpRequest.Builder builder = HttpRequest.newBuilder().uri(URI.create(serverURL + path));
+        builder.setHeader("Authorization", authToken);
+        if (body != null) {
+            builder.method(method, HttpRequest.BodyPublishers.ofString(new Gson().toJson(body)));
+            builder.setHeader("Content-Type", "aaplication/json");
+        }
+        return builder.build();
+    }
+
+    public HttpResponse<String> sendRequest(HttpRequest request) throws ResponseException {
+        try {
+            return client.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (Exception ex) {
+            throw new ResponseException(" So sorry, we are currently having issues with sending messages to the CGI server."
+                    + "\n " + ex.getMessage(), 0);
+        }
+    }
+
+    public <T> T handleResponse(HttpResponse<String> response, Class<T> responseClass) throws ResponseException {
+        if (response.statusCode() != 200) {
+            String jsonBody = response.body();
+            if (jsonBody != null) {
+                throw new ResponseException(new Gson().fromJson(jsonBody, ErrorResult.class).message(), 0);
+            }
+            throw new ResponseException(" Failed to get error message", 0);
+        }
+        if (responseClass != null) {
+            return new Gson().fromJson(response.body(), responseClass);
+        }
+        return null;
+    }
+}
